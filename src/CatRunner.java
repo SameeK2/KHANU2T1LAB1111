cd8442715d149feef69e3f289f0e4252179abc44
Samee Khan <sameek2@nycstudents.net>
2023-09-28 09:24:12 -0400

    Added Cat and Cat Runner

---
+++ src/CatRunner.java
@@ -0,0 +1,18 @@
+public class CatRunner {
+    public static void main(String[] arg){
+        Cat cat1 = new Cat("Bob", 4, 12.0);
+        Cat cat2 = new Cat("john", 5, 14.0);
+        Cat cat3 = new Cat("Billy", 8 , 15.0);
+
+        cat1.printCatInfo();
+        cat1.introduce();
+
+        cat2.printCatInfo();
+        cat2.introduce();
+
+
+
+
+
+    }
+}
